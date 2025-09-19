3a236ea1229ecbd38dab69f5dd868ec72f3393cf
opilane <opilane@HP-CZC2349HT4>
2025-09-19 11:36:52 +0300

    .

---
+++ OOP_põhialused/Sõiduk_ülesanne/ISõiduk.cs
@@ -0,0 +1,15 @@
+﻿using OOP_põhialused;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_põhialused
+{
+    public interface ISõiduk
+    {
+        double ArvutaKulu();
+        double ArvutaVahemaa();
+    } 
+}
