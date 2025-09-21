ca3c635b87fdd5534fa6a07ed36d6d3b7d7bc0e7
ariia Posvystak <posvystakmariia@gmail.com>
2025-09-21 19:58:40 +0300

    .

---
+++ OOP_põhialused/Funk.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_põhialused.Sõiduk_ülesanne
+{
+    internal class Funk
+    {
+        public static void LaadiFailist(List<ISõiduk> sõidukid, string failinimi)
+        {
+            foreach (var rida in File.ReadAllLines(failinimi))
+            {
+                var osad = rida.Split(';');
+                if (osad.Length < 2) continue;
+
+                string tüüp = osad[0].ToLower();
+                ISõiduk s = null;
+
+                switch (tüüp)
+                {
+                    case "auto":
+                        if (osad.Length >= 3 &&
+                            double.TryParse(osad[1], out double aKulu) &&
+                            double.TryParse(osad[2], out double aKm))
+                            s = new Auto(aKulu, aKm);
+                        break;
+                    case "jalgratas":
+                        if (double.TryParse(osad[1], out double jKm))
+                            s = new Jalgratas(jKm);
+                        break;
+                    case "buss":
+                        if (osad.Length >= 4 &&
+                            double.TryParse(osad[1], out double bKulu) &&
+                            double.TryParse(osad[2], out double bKm) &&
+                            int.TryParse(osad[3], out int r))
+                            s = new Buss(bKulu, bKm, r);
+                        break;
+                    case "elektritõukeratas":
+                        if (osad.Length >= 3 &&
+                            double.TryParse(osad[1], out double eKulu) &&
+                            double.TryParse(osad[2], out double eKm))
+                            s = new Elektritõukeratas(eKulu, eKm);
+                        break;
+                }
+
+                if (s != null) sõidukid.Add(s);
+            }
+
+            Console.WriteLine("Andmed failist laaditud!");
+        }
+
+        public static void SalvestaFaili(List<ISõiduk> sõidukid, string failinimi)
+        {
+            using (StreamWriter sw = new StreamWriter(failinimi))
+            {
+                foreach (var s in sõidukid)
+                {
+                    if (s is Auto auto)
+                        sw.WriteLine($"auto;{auto.ArvutaKulu() * 100 / auto.ArvutaVahemaa()};{auto.ArvutaVahemaa()}");
+                    else if (s is Jalgratas jalgratas)
+                        sw.WriteLine($"jalgratas;{jalgratas.ArvutaVahemaa()}");
+                    else if (s is Buss buss)
+                        sw.WriteLine($"buss;{buss.ArvutaKulu() * buss.ArvutaVahemaa() / 100};{buss.ArvutaVahemaa()};1");
+                    else if (s is Elektritõukeratas tõuks)
+                        sw.WriteLine($"elektritõukeratas;{tõuks.ArvutaKulu() * 100 / tõuks.ArvutaVahemaa()};{tõuks.ArvutaVahemaa()}");
+                }
+            }
+        }
+    }
+}
---
+++ OOP_põhialused/Start_Sõiduk.cs
@@ -0,0 +1,91 @@
+﻿using OOP_põhialused.Sõiduk_ülesanne;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_põhialused
+{
+    internal class Start_Sõiduk
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                List<ISõiduk> sõidukid = new List<ISõiduk>();
+
+                Console.WriteLine("Kas soovid laadida andmed failist (jah/ei)?");
+                string laadiFailist = Console.ReadLine()?.Trim().ToLower();
+
+                string failinimi = @"..\..\..\andmed.txt";
+
+                if (laadiFailist == "jah" && File.Exists(failinimi))
+                {
+                    Funk.LaadiFailist(sõidukid, failinimi);
+                }
+
+                while (true)
+                {
+                    Console.WriteLine("Vali sõiduk: 1=Auto, 2=Jalgratas, 3=Buss, 4=Elektritõukeratas, 0=Välju");
+                    string valik = Console.ReadLine();
+
+                    if (valik == "0") break;
+
+                    switch (valik)
+                    {
+                        case "1":
+                            Console.Write("Kütusekulu (l/100km): ");
+                            double aKulu = double.Parse(Console.ReadLine());
+                            Console.Write("Vahemaa (km): ");
+                            double aKm = double.Parse(Console.ReadLine());
+                            sõidukid.Add(new Auto(aKulu, aKm));
+                            break;
+                        case "2":
+                            Console.Write("Vahemaa (km): ");
+                            double jKm = double.Parse(Console.ReadLine());
+                            sõidukid.Add(new Jalgratas(jKm));
+                            break;
+                        case "3":
+                            Console.Write("Kütusekulu (l/100km): ");
+                            double bKulu = double.Parse(Console.ReadLine());
+                            Console.Write("Vahemaa (km): ");
+                            double bKm = double.Parse(Console.ReadLine());
+                            Console.Write("Reisijate arv: ");
+                            int r = int.Parse(Console.ReadLine());
+                            sõidukid.Add(new Buss(bKulu, bKm, r));
+                            break;
+                        case "4":
+                            Console.Write("Energiakulu (kWh/100km): ");
+                            double eKulu = double.Parse(Console.ReadLine());
+                            Console.Write("Vahemaa (km): ");
+                            double eKm = double.Parse(Console.ReadLine());
+                            sõidukid.Add(new Elektritõukeratas(eKulu, eKm));
+                            break;
+                        default:
+                            Console.WriteLine("Vale valik!");
+                            break;
+                    }
+                }
+
+                // --- вывод всех результатов ---
+                Console.WriteLine("--- Sõidukite tulemused ---");
+                foreach (var s in sõidukid)
+                {
+                    Console.WriteLine($"{s} | Kulu: {s.ArvutaKulu():F2} | Vahemaa: {s.ArvutaVahemaa():F2}");
+                }
+
+                double koguKulu = sõidukid.Sum(s => s.ArvutaKulu());
+                Console.WriteLine($"Kogu kulu: {koguKulu:F2}");
+
+                // --- сохранение в файл ---
+                Funk.SalvestaFaili(sõidukid, failinimi);
+                Console.WriteLine($"Andmed salvestatud faili: andmed.txt");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Viga: palun sisesta numbrid õigesti!");
+            }
+        }
+    }
+}
